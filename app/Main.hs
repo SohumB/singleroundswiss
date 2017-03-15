425aedbd60634aab9290d943d79ec80ec5249dc0
Sohum Banerjea <sohumb@gmail.com>
2017-03-15 13:39:00 -0700

    pull parameterisation out a bit

--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,16 @@ module Main where
 
 import Lib (Tournament, Pairing, empty, step, pair, pairMinimising, players, corpDist, mispairDist)
 
-tournament :: Int -> Pairing -> IO Tournament
-tournament rounds pfn = foldr (flip (>>=)) (return $ empty 32) (replicate rounds (step pfn))
+tournament :: Int -> Int -> Pairing -> IO Tournament
+tournament rounds players pfn = foldr (flip (>>=)) (return $ empty players) (replicate rounds (step pfn))
 
-runSimulation :: Pairing -> IO ()
-runSimulation pfn = do
-  ts <- sequence $ replicate 10000 (tournament 8 pfn)
+runSimulation :: Int -> Pairing -> IO ()
+runSimulation n pfn = do
+  ts <- sequence $ replicate n (tournament 8 32 pfn)
   let ps = concatMap players ts
   putStrLn (show (corpDist ps) ++ ", " ++ show (mispairDist ts) ++ " mispairs")
 
 main :: IO ()
 main = do
-  runSimulation pair
-  runSimulation pairMinimising
+  runSimulation 10000 pair
+  runSimulation 10000 pairMinimising
